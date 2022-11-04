412b7677f25cb609bb5b660837f9afcce08729e0
Beatriz Leite <41204@ufp.edu.pt>
2022-11-04 11:57:47 +0000

    first commit, projeto iniciado

---
+++ codigo/numerosBipolares.c
@@ -0,0 +1,5 @@
+//
+// Created by Bea on 04/11/2022.
+//
+
+#include "../headers/numerosBipolares.h"
---
+++ main.c
@@ -0,0 +1,10 @@
+//
+// Created by Bea on 04/11/2022.
+//
+#include <stdio.h>
+#include "headers/numerosBipolares.h"
+
+int main(int argc, const char *argv[]) {
+    printf("Projeto LP1/AED1!\n");
+    return 0;
+}
